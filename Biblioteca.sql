b9b0494e2cdada8928beeecf7c6e7d2069328df6
Matheus Henrique <matheus.braz@transformatechadvisors.com>
2025-11-03 17:19:57 -0300

    adicionando segunda entrega - código

---
+++ Biblioteca.sql
@@ -0,0 +1,165 @@
+CREATE TABLE Usuario(
+    id_usuario INT,
+    nome VARCHAR(45),
+    email VARCHAR(100),
+    telefone VARCHAR(45),
+    data DATE,
+
+    PRIMARY KEY (id_usuario)
+);
+
+CREATE TABLE Autor(
+    id_autor INT,
+    nome_autor VARCHAR(45),
+    nascionalidade VARCHAR(45),
+
+    primary key (id_autor)
+);
+
+CREATE TABLE Livro(
+    id_livro INT,
+    titulo VARCHAR(45),
+    ano INT,
+
+    primary key (id_livro)
+);
+
+CREATE TABLE Autor_has_Livro(
+    id_autor INT,
+    id_livro INT,
+
+    PRIMARY KEY (id_autor, id_livro),
+
+    FOREIGN KEY (id_autor) REFERENCES Autor(id_autor),
+    FOREIGN KEY (id_livro) REFERENCES Livro(id_livro)
+);
+
+CREATE TABLE Categoria(
+    livro_usuario VARCHAR(45),
+    nomeCategoria VARCHAR(45),
+    descricao TEXT,
+
+    primary key (livro_usuario)
+ );
+
+CREATE TABLE Emprestimo(
+    id_emprestimo INT,
+    id_usuario INT,
+    dataEmprestimo DATE,
+    dataDevolucao DATE,
+    status BIT, --Entregue ou não entregue
+
+    primary key (id_emprestimo, id_usuario)
+);
+
+INSERT INTO Usuario
+values
+(1, "Matheus", "matheus@inatel.br", "(95) 9875-4521", "03/08/2025"),
+(2, "Solange", "sol@inatel.br", "(22) 7954-6344", "16/12/2023"),
+(3, "Warley", "warley@inatel.br", "(67) 5347-7733", "22/06/2020");
+
+INSERT INTO Autor
+values
+(1, "Edgar Allan Poe", "Estados Unidos"),
+(2, "Machado de Assis", "Brasil"),
+(3, "Monteiro Lobato", "Brasil");
+
+INSERT INTO Livro
+values
+(84, "O Gato Preto", 1843),
+(26, "Memorias Postumas de Brás Cubas", 1881),
+(44, "Reinações de Narizinho", "1931");
+
+INSERT INTO Categoria
+values
+("O Gato Preto", "Terror", "Um conto sombrio de Edgar Allan Poe que explora a loucura, a culpa e o peso da consciência humana."),
+("Memorias Postumas de Brás Cubas", "Romance", "Uma narrativa irônica e filosófica em que um defunto narra sua própria vida e critica a sociedade do século XIX."),
+("Reinações de Narizinho", "Infanto-juvenil", "Uma encantadora história infantil de Monteiro Lobato que mistura fantasia, imaginação e personagens do Sítio do Picapau Amarelo.");
+
+INSERT INTO Emprestimo
+values
+(12, 1, "03/09/2025", "03/10/2025", 1),
+(25, 2, "01/01/2024", "01/02/2024", 0),
+(66, 3, "23/10/2021", "23/11/2025", 0);
+
+-- Atualizar o e-mail de um usuário
+UPDATE Usuario
+SET email = 'matheus.henrique@inatel.edu.br'
+WHERE id_usuario = 1;
+
+-- Atualizar o status de um empréstimo (livro devolvido)
+UPDATE Emprestimo
+SET status = 1
+WHERE id_emprestimo = 25 AND id_usuario = 2;
+
+-- Deletar um livro (exemplo de remoção total)
+DELETE FROM Livro
+WHERE id_livro = 84; -- “O Gato Preto”
+
+--  Deletar um autor (exemplo de exclusão completa)
+DELETE FROM Autor
+WHERE id_autor = 3; -- “Monteiro Lobato”
+
+ALTER TABLE Usuario
+ADD cpf VARCHAR(14);
+
+DROP TABLE Autor_has_Livro;
+
+-- Criar um novo usuário
+-- Conceder privilégios para o usuário
+-- Atualiza imediatamente os privilégios
+CREATE USER 'bibliotecario'@'localhost' IDENTIFIED BY 'senha123';
+GRANT ALL PRIVILEGES ON *.* TO 'bibliotecario'@'localhost';
+FLUSH PRIVILEGES;
+
+--Function: calculando o total de emprestimos
+DELIMITER //
+CREATE FUNCTION totalEmprestimos(id INT)
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    DECLARE total INT;
+    SELECT COUNT(*) INTO total
+    FROM Emprestimo
+    WHERE id_usuario = id;
+    RETURN total;
+END //
+DELIMITER ;
+
+SELECT totalEmprestimos(1) AS 'Total de empréstimos de Matheus';
+
+-- Cadastrando um novo livro
+DELIMITER //
+CREATE PROCEDURE cadastrarLivro(
+    IN p_id_livro INT,
+    IN p_titulo VARCHAR(45),
+    IN p_ano INT,
+    IN p_nomeCategoria VARCHAR(45),
+    IN p_descricao TEXT
+)
+BEGIN
+    INSERT INTO Livro (id_livro, titulo, ano)
+    VALUES (p_id_livro, p_titulo, p_ano);
+
+    INSERT INTO Categoria (livro_usuario, nomeCategoria, descricao)
+    VALUES (p_id_livro, p_nomeCategoria, p_descricao);
+END //
+DELIMITER ;
+
+CALL cadastrarLivro(99, 'O Alienista', 1882, 'Sátira', 'Uma crítica bem-humorada à loucura e à sociedade da época.');
+
+-- Lista todos os emprestimos - VIEW
+CREATE VIEW vw_emprestimos_detalhados AS
+SELECT
+    e.id_emprestimo,
+    u.nome AS nome_usuario,
+    e.dataEmprestimo,
+    e.dataDevolucao,
+    CASE
+        WHEN e.status = 1 THEN 'Entregue'
+        ELSE 'Pendente'
+    END AS status_livro
+FROM Emprestimo e
+JOIN Usuario u ON e.id_usuario = u.id_usuario;
+
+SELECT * FROM vw_emprestimos_detalhados;
